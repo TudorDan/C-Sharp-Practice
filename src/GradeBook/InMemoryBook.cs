743a9870b4bce993f460e2843aa5ab537e4d3d10
TudorDan <tudor.ist@gmail.com>
2020-10-11 14:37:32 +0300

    Add IO practice

--- src/GradeBook/InMemoryBook.cs
+++ src/GradeBook/InMemoryBook.cs
@@ -61,10 +61,7 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
-
+            /*
             foreach (var grade in grades)
             {
                 if (grade == 42.1)
@@ -76,31 +73,11 @@ namespace GradeBook
                     // skip over this
                     continue;
                 }
-
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
             }
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
+            */
+            for (var index = 0; index < grades.Count; index++)
             {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
+                result.Add(grades[index]);
             }
             
             return result;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,8 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new InMemoryBook("Scott's Grade Book");
+            IBook book = new DiskBook("Scott's Grade Book");
+            // var book = new InMemoryBook("Scott's Grade Book");
             book.GradeAdded += OnGradeAdded;
             book.GradeAdded -= OnGradeAdded;
             book.GradeAdded += OnGradeAdded;
@@ -15,6 +16,7 @@ namespace GradeBook
             var stats = book.GetStatistics();
 
             Console.WriteLine(InMemoryBook.CATEGORY);
+            Console.WriteLine($"From the book called {book.Name}:");
             Console.WriteLine($"The lowest grade is {stats.Low}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
